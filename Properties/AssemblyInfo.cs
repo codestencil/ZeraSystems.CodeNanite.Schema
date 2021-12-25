23e1ff89c42b5ec5b66b04195821b7c737b5c1ec
Ayo Dahunsi <ayo@dolasoft.com>
2021-12-25 05:04:16 -0700

    Updated expansion nuget package

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ZERA Systems Inc.")]
 [assembly: AssemblyProduct("CodeStencil CodeNanites")]
-[assembly: AssemblyCopyright("Copyright ©  2021 ZERA Systems Inc.")]
+[assembly: AssemblyCopyright("Copyright ©  2022 ZERA Systems Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.3.9")]
-[assembly: AssemblyFileVersion("1.3.3.9")]
+[assembly: AssemblyVersion("1.3.4.0")]
+[assembly: AssemblyFileVersion("1.3.4.0")]
